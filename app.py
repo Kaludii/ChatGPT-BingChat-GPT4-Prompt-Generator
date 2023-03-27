0de52e05d78604b628837c734ad96e8fec7a8ecc
Bilal Kaludi <63890666+Kaludii@users.noreply.github.com>
2023-03-27 17:13:20 -0500

    updated to a new model and added slider for max tokens

--- app.py
+++ app.py
@@ -2,20 +2,21 @@ import streamlit as st
 import random
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-tokenizer = AutoTokenizer.from_pretrained("merve/chatgpt-prompts-bart-long")
-model = AutoModelForSeq2SeqLM.from_pretrained("merve/chatgpt-prompts-bart-long", from_tf=True)
+tokenizer = AutoTokenizer.from_pretrained("Kaludi/chatgpt-gpt4-prompts-bart-large-cnn-samsum")
+model = AutoModelForSeq2SeqLM.from_pretrained("Kaludi/chatgpt-gpt4-prompts-bart-large-cnn-samsum", from_tf=True)
 
-def generate(prompt):
+def generate(prompt, max_new_tokens):
     batch = tokenizer(prompt, return_tensors="pt")
-    generated_ids = model.generate(batch["input_ids"], max_new_tokens=150)
+    generated_ids = model.generate(batch["input_ids"], max_new_tokens=max_new_tokens)
     output = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
     return output[0]
 
 st.title("ChatGPT-BingChat Prompt Generator")
-st.write("This app generates ChatGPT/BingChat & GPT-3 prompts using [this](https://huggingface.co/merve/chatgpt-prompts-bart-long) model trained by Merve. Enter a role and a prompt will be generated based on it.")
+st.write("This app generates ChatGPT/BingChat & GPT-3 prompts using [this](https://huggingface.co/Kaludi/chatgpt-gpt4-prompts-bart-large-cnn-samsum) model trained by [Kaludi](https://huggingface.co/Kaludi/). Enter a role and a prompt will be generated based on it.")
 prompt = st.text_input("Enter a Role, Example: Virtual Assistant", placeholder="Text here", value="")
+max_new_tokens = st.slider("Select Max Tokens in Response", min_value=100, max_value=500, value=150, step=10)
 if st.button("Generate"):
-    output = generate(prompt)
+    output = generate(prompt, max_new_tokens)
     st.write("Generated Prompt:", box=True)
     st.write("<div style='background-color: #2E2E2E; padding: 10px;'>{}</div>".format(output), unsafe_allow_html=True)
 st.write("")
